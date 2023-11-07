b94b5575d9b4c2e4af4f335c89dc47ce19ad957a
Philipekum <filipp.tsekhansky@yandex.ru>
2023-11-07 23:03:08 +0300

    inital

--- Wine/WineAnalyzer/views.py
+++ Wine/WineAnalyzer/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+menu = ["О сайте", "Добавить статью", "Обратная связь"]
+
+
+def index(request):
+    return render(request, 'WineAnalyzer/index.html', {'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'WineAnalyzer/about.html', {'menu': menu, 'title': 'О сайте'})
+
