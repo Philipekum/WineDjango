7a0c48d820fd0ed418b4546243b5576dfb6b7e2f
Philipp <filipp.tsekhansky@yandex.ru>
2023-11-08 09:09:36 +0300

    working on analyzer page

--- Wine/WineAnalyzer/views.py
+++ Wine/WineAnalyzer/views.py
@@ -8,4 +8,28 @@ def home(request):
 
 
 def analyzer(request):
-    return render(request, 'WineAnalyzer/analyzer.html')
+    if request.method == 'POST':
+        print('!!!')
+        fa = request.POST.get('fixed acidity')
+        va = request.POST['volatile acidity']
+        ca = request.POST['citric acid']
+        rs = request.POST['residual sugar']
+        ch = request.POST['chlorides']
+        fsd = request.POST['free sulfur dioxide']
+        tsd = request.POST['total sulfur dioxide']
+        dens = request.POST['density']
+        ph = request.POST['pH']
+        sulp = request.POST['sulphates']
+        alc = request.POST['alcohol']
+
+        result = list(map(float, [fa, va, ca, rs, ch, fsd, tsd, dens, ph, sulp, alc]))
+
+    else:
+        result = -1
+
+    return render(request, 'WineAnalyzer/analyzer.html',
+                  {'acids':           ['fixed acidity', 'volatile acidity', 'citric acid'],
+                   'sugar_chlorides': ['residual sugar', 'chlorides'],
+                   'sulfur_dioxides': ['free sulfur dioxide', 'total sulfur dioxide'],
+                   'others':          ['density', 'pH', 'sulphates', 'alcohol'],
+                   'result':          result})
