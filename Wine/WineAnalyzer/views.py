c2c170cee151f0cd1feeb2c31410417f305432b8
Philipp <filipp.tsekhansky@yandex.ru>
2023-11-08 04:38:57 +0300

    frontend

--- Wine/WineAnalyzer/views.py
+++ Wine/WineAnalyzer/views.py
@@ -3,10 +3,5 @@ from django.shortcuts import render
 menu = ["О сайте", "Добавить статью", "Обратная связь"]
 
 
-def index(request):
-    return render(request, 'WineAnalyzer/index.html', {'menu': menu, 'title': 'Главная страница'})
-
-
-def about(request):
-    return render(request, 'WineAnalyzer/about.html', {'menu': menu, 'title': 'О сайте'})
-
+def home(request):
+    return render(request, 'WineAnalyzer/home.html')
